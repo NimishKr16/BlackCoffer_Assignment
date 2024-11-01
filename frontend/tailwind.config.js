b7f59bbbe93a20d5bd8e5c32cb0e258883f787ca
Nimish Kumar <nimishkumar0305@gmail.com>
2024-11-01 14:04:23 +0530

    Add chartjs

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all your JSX/TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
